e478ccf04e25f42c32d693f06248eb548aa998d3
Ninad <telangn@gmail.com>
2018-01-22 22:24:29 -0800

    added padding to header with Js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 /* Show and hide menu */
 /*jslint browser: true*/
-/*global $, document, location, window */
+/*global $, document, location, window, setInterval */
 
 /*$(document).ready(function(){*/
     $(function(){
@@ -84,5 +84,59 @@
       });
 
     });
+    
+    //active menu on click
+    $(document).ready(function(){
+        'use strict';
+       
+        $('.navbar-nav li a').click(function(){
+            'use strict';
+            $('.navbar-nav li a').parent().removeClass("active");
+            $(this).parent().addClass("active");
+        });
         
+        
+    });
+      
+    //highlight menu item on scroll
+    $(document).ready(function(){
+       'use strict';
+        
+        $(window).scroll(function(){
+           'use strict';
+            
+            $("section").each(function(){
+            'use strict';
+            
+            var bb = $(this).attr("id");
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            
+            if($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei){
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+            }   
+            });
+        });
+    });
+        
+    //add auto padding to header
+    $(document).ready(function(){
+        'use strict';
+        
+        setInterval(function(){
+           'use strict'; 
+            
+            var windowHeight = $(window).height();
+            var containerHeight = $(".header-container").height();
+            var padTop = windowHeight - containerHeight;
+            
+            $(".header-container").css({
+               
+                'padding-top': Math.round( padTop / 2 ) + 'px',
+                'padding-bottom': Math.round( padTop / 2 ) + 'px' 
+            });
+        }, 10)
+    });
 });
