428dc6ca36070dbe9fd66e9d606deb907d42f377
Ninad <telangn@gmail.com>
2018-01-24 10:04:33 -0800

    animation for counter

--- js/custom.js
+++ js/custom.js
@@ -154,4 +154,15 @@
         });
         
     });
+        
+    // Add Counter
+    $(document).ready(function() {
+        $('.counter-num').counterUp({
+           
+            delay: 10,
+            time: 2000
+            
+        });
+        
+    });
 });
